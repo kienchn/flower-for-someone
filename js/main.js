fa35e8671f5ac9f8d82e0d51a36aef6723d763db
kienchn <kiensalalima34@gmailcom>
2024-11-13 01:13:07 +0800

    Initial commit of my flower

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('I LOVE U').split('')
+    const titles = ('This flower is for you because you are the most beautiful and kind girl').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
